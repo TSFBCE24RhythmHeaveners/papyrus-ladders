1202e838dc2b052c28006ce1b8e4455c42e912a1
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-01-29 20:07:54 +0400

    instead of every block, the papyrus.

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 minetest.register_on_mods_loaded(function()
-	for node,def in pairs(minetest.registered_nodes) do
+	for node,papyrus in pairs(minetest.registered_nodes) do
 		minetest.override_item(node,{
 			walkable = false,
 			climbable = true,
