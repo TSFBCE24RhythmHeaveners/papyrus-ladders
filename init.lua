06f6accfcd8e5a250f54aec60f39ed5fe45ba1cb
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-01-30 19:09:48 +0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 minetest.register_on_mods_loaded(function()
 	for node,def in pairs(minetest.registered_nodes) do
-		if def.default:papyrus > 0 then
+		if def.plantlife.papyrus and def.plantlife.papyrus and def.plantlife.papyrus > 0 then
 			minetest.override_item(node,{
 				walkable = false,
 				climbable = true,
